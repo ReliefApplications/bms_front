c0ad736c8e630883dc071a86fc8e50e21ef912f3
Julia Meyer <julia.reliefapps@gmail.com>
2018-06-21 12:07:10 +0200

    add distribution and http services

---
+++ src/app/core/api/distribution.service.ts
@@ -0,0 +1,16 @@
+// import { Injectable             } from '@angular/core';
+// import { URL_BMS_API } from '../../../environments/environment';
+
+// @Injectable({
+// 	providedIn: 'root'
+// })
+// export class DistributionService{
+//     readonly api = URL_BMS_API;
+
+//     constructor(){}
+
+//     public get() {
+//         let url = this.api + "/distribution";
+//         return this.http.get(url);
+//     }
+// }

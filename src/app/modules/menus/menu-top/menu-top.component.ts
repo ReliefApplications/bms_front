aeb887092fe050c59b8d8634e6ad04c3d23bb8f8
Julia Meyer <julia.reliefapps@gmail.com>
2018-06-21 10:06:27 +0200

    close opened menu when click

--- src/app/modules/menus/menu-top/menu-top.component.ts
+++ src/app/modules/menus/menu-top/menu-top.component.ts
@@ -9,6 +9,7 @@ import { Router, NavigationEnd } from '@angular/router';
 export class MenuTopComponent implements OnInit {
   @Input() openMenu;
   @Output() emitCloseMenu = new EventEmitter<boolean>();
+  @Output() emitCurrentRoute = new EventEmitter<string>();
   public activeTitle = "";
 
   constructor(
@@ -19,8 +20,14 @@ export class MenuTopComponent implements OnInit {
     this.router.events.subscribe(e => {
       if (e instanceof NavigationEnd) {
         this.activeTitle = e.url;
+        this.emitCurrentRoute.emit(this.activeTitle);
       }
     })
+  }
+
+  setCurrentRoute(route){
+    this.activeTitle = "/"+route;
+    this.emitCloseMenu.emit(true);
   }
   
   closeTopMenu(){

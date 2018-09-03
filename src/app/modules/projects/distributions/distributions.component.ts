fdf1002bcddb1e2ee37f61c3dfa33d6a30b77594
Alexandre Moray <alexandre.reliefapps@gmail.com>
2018-09-03 17:12:52 +0200

    first commit +component created

---
+++ src/app/modules/projects/distributions/distributions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-distributions',
+  templateUrl: './distributions.component.html',
+  styleUrls: ['./distributions.component.scss']
+})
+export class DistributionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

75a0c7922f5f81fd821f59aaffa26ec2148a1c3a
Alexandre Moray <alexandre.reliefapps@gmail.com>
2018-09-27 17:02:09 +0200

    fixed v2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { ProfileComponent } from './modules/profile/profile.component';
 import { HeaderMobileComponent } from './components/headers/header-mobile/header-mobile.component';
 import { HeaderComponent } from './components/headers/header/header.component';
 import { ModalLeaveComponent } from './components/modals/modal-leave/modal-leave.component';
+import { PlaceholderDashboardComponent } from './placeholders/placeholder-dashboard/placeholder-dashboard.component';
 
 @NgModule({
 	declarations: [
@@ -31,6 +32,7 @@ import { ModalLeaveComponent } from './components/modals/modal-leave/modal-leave
     	HeaderMobileComponent,
 		HeaderComponent,
 		ModalLeaveComponent,
+		PlaceholderDashboardComponent,
 	],
 	imports: [
 		// Modules

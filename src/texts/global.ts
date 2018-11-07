71359e86aa8d87a6e088ccb0bf7c201094d60775
Mathias <mathias.reliefapps@gmail.com>
2018-11-07 11:39:02 +0100

    Set RTL to the html tag, set a margin and change side of the add-button when we use the arabic language

--- src/texts/global.ts
+++ src/texts/global.ts
@@ -1,25 +1,43 @@
-import { TEXT as TEXT_FR} from "./global_fr";
-import { TEXT as TEXT_EN} from "./global_en";
-import { TEXT as TEXT_AR} from "./global_ar";
+import { TEXT as TEXT_FR } from "./global_fr";
+import { TEXT as TEXT_EN } from "./global_en";
+import { TEXT as TEXT_AR } from "./global_ar";
 
 export class GlobalText {
     static language = 'en';
     static languages = ['en', 'fr', 'ar'];
     static TEXTS = TEXT_EN;
-   
+
     static maxHeight = 700;
     static maxWidthMobile = 750;
     static maxWidthFirstRow = 1000;
     static maxWidthSecondRow = 800;
     static maxWidth = 750;
-    
-    public static changeLanguage(language : string){
-       GlobalText.language = language;
-       switch(language){
-        case 'en':  GlobalText.TEXTS = TEXT_EN; break;
-        case 'fr': GlobalText.TEXTS = TEXT_FR; break;
-        case 'ar': GlobalText.TEXTS = TEXT_AR; break;
-        default: GlobalText.TEXTS = TEXT_EN; break;
+
+    public static changeLanguage(language: string) {
+        GlobalText.language = language;
+        switch (language) {
+            case 'en':
+                GlobalText.TEXTS = TEXT_EN;
+                document.getElementsByTagName('html')[0].setAttribute('dir', '');
+                document.getElementsByTagName("mat-sidenav-content")[0].style.margin = "0px 0px 0px 64px";
+                document.getElementsByClassName("add-buttons")[0].style.right = "3vh";
+                document.getElementsByClassName("add-buttons")[0].style.left = "inherit";
+                break;
+            case 'fr':
+                GlobalText.TEXTS = TEXT_FR;
+                document.getElementsByTagName('html')[0].setAttribute('dir', '');
+                document.getElementsByTagName("mat-sidenav-content")[0].style.margin = "0px 0px 0px 64px";
+                document.getElementsByClassName("add-buttons")[0].style.right = "3vh";
+                document.getElementsByClassName("add-buttons")[0].style.left = "inherit";
+                break;
+            case 'ar':
+                GlobalText.TEXTS = TEXT_AR;
+                document.getElementsByTagName('html')[0].setAttribute('dir', 'rtl');
+                document.getElementsByTagName("mat-sidenav-content")[0].style.margin = "0px 64px 0px 0px";
+                document.getElementsByClassName("add-buttons")[0].style.right = "inherit";
+                document.getElementsByClassName("add-buttons")[0].style.left = "3vh";
+                break;
+            default: GlobalText.TEXTS = TEXT_EN; break;
         }
     }
 }

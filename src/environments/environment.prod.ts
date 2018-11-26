64faa80da15674a22853403a6b7b399fdb2716ae
Barbara <barbara.reliefapps@gmail.com>
2018-11-26 16:12:44 +0100

    changed environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,4 @@ export const environment = {
   production: true
 };
 
-export const URL_BMS_API    = 'http://bms-env.uu2iu523n9.eu-central-1.elasticbeanstalk.com/api/wsse';
+export const URL_BMS_API    = 'https://api.bmstaging.info/api/wsse';

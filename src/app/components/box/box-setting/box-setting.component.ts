aebf3986b813cffda6a39f955cdcac35824493bf
Alexandre Moray <alexandre.reliefapps@gmail.com>
2018-12-10 11:22:34 +0100

    fixed distribution page responsiveness + boxes names responsiveness length

--- src/app/components/box/box-setting/box-setting.component.ts
+++ src/app/components/box/box-setting/box-setting.component.ts
@@ -1,29 +1,30 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'app-box-setting',
-  templateUrl: './box-setting.component.html',
-  styleUrls: ['./box-setting.component.scss']
+    selector: 'app-box-setting',
+    templateUrl: './box-setting.component.html',
+    styleUrls: ['./box-setting.component.scss']
 })
 export class BoxSettingComponent {
-  @Input() selectedTitle;
-  @Input() info: any;
-  @Output() emitClickedTitle = new EventEmitter<string>();
+    @Input() selectedTitle;
+    @Input() info: any;
+    @Output() emitClickedTitle = new EventEmitter<string>();
 
-  ngOnInit() {
-    if(this.info.ref === "users")
-    {
-      this.selectedTitle = "users";
-    }
-     else if(this.info.icon === "settings/projects" && !this.selectedTitle) {
-      this.selectedTitle = this.info.ref;
-    }
-    else if (this.info.ref === "file import") {
-      this.selectedTitle = "file import";
-    }
-  }
+    readonly MAX_PROP_LENGTH = 35;
 
-  emitTitle(title) {
-    this.emitClickedTitle.emit(title);
-  }
+    ngOnInit() {
+        if (this.info.ref === "users") {
+            this.selectedTitle = "users";
+        }
+        else if (this.info.icon === "settings/projects" && !this.selectedTitle) {
+            this.selectedTitle = this.info.ref;
+        }
+        else if (this.info.ref === "file import") {
+            this.selectedTitle = "file import";
+        }
+    }
+
+    emitTitle(title) {
+        this.emitClickedTitle.emit(title);
+    }
 }

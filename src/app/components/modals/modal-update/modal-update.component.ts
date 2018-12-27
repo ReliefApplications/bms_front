c113529ecc152ee09b98ae799f030e0a91bf9a0a
Mathias <mathias.reliefapps@gmail.com>
2018-12-27 11:46:18 +0100

    preselect Modality when add new commodities in adding distributions

--- src/app/components/modals/modal-update/modal-update.component.ts
+++ src/app/components/modals/modal-update/modal-update.component.ts
@@ -24,7 +24,7 @@ export class ModalUpdateComponent extends ModalComponent {
         } catch (e) {
             console.error('the function mapAllProperties is not defined for the entity ', this.entityInstance);
         }
-
+        this.loadData(this.updateObject);
         if (this.updateObject.email && this.updateObject.username) {
             this.form.controls['emailFormControl'].disable();
 

7c03077f26a80b36f9858e56b065e3a141f4564c
Francis Wardley <fwardley90@gmail.com>
2019-01-10 10:27:29 +0100

    fixed

--- src/texts/global_en.ts
+++ src/texts/global_en.ts
@@ -313,7 +313,7 @@ export const TEXT = {
     project_description: 'You will be able to modify project\'s name until your first distribution.',
     project_go_beneficiaries: 'Go to Beneficiaries',
     project_no_distribution: 'This project does not contain any distributions. Create your first one !',
-    project_no_household: 'This project does not contain any household.',
+    project_no_household: 'This project does not contain any households.',
     project_no_projects: 'This country does not contain any project.',
 
     // Report

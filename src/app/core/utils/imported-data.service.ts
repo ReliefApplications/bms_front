e0f6e7764df34469c3904c186bdfaf52ed181e94
Francis Wardley <fwardley90@gmail.com>
2019-01-14 11:31:36 +0100

    removed Output and Event Emitter

--- src/app/core/utils/imported-data.service.ts
+++ src/app/core/utils/imported-data.service.ts
@@ -1,11 +1,9 @@
-import {Injectable, Output, EventEmitter} from '@angular/core';
+import {Injectable} from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ImportedDataService {
-
-  @Output() fire: EventEmitter<any> = new EventEmitter();
 
   data: any;
   project: any;

205cdbe33c549612bdb2dc2ac88eded473c7a31b
ibrahim <ibrahim.reliefapps@gmail.com>
2019-01-15 14:18:47 +0100

    Move ‘Projects’ question to ‘Done’ page, to make it more obvious

--- src/texts/global_ar.ts
+++ src/texts/global_ar.ts
@@ -37,6 +37,7 @@ export const TEXT = {
     rights: 'الحقوق',
     save: 'حفظ',
     settings: 'الإعدادات',
+    summary:'Summary',
     update: 'تحديث',
 
     // Error
--- src/texts/global_fr.ts
+++ src/texts/global_fr.ts
@@ -37,6 +37,7 @@ export const TEXT = {
     rights: 'Rôles',
     save: 'Enregistrer',
     settings: 'Paramètres',
+    summary:'Sommaire',
     update: 'Modifier',
 
     // Error

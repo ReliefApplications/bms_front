776a4e8ab4fd4122576d1203ad38877578be8253
developper <developper@MacBook-Pro-de-Mathias.local>
2019-02-06 12:16:59 +0100

    Change Date of Distribution to Distribution Start Date

--- src/texts/global_fr.ts
+++ src/texts/global_fr.ts
@@ -291,7 +291,7 @@ export const TEXT = {
     model_criteria_operator: 'Condition',
     model_criteria_weight: 'Score du poids',
     model_dateofbirth: 'Date de naissance',
-    model_distribution_date: 'Date de la Distribution',
+    model_distribution_date: 'Date Début Distribution',
     model_distribution_name: 'Nom',
     model_distribution_type: 'Cible',
     model_donor: 'Donneur',

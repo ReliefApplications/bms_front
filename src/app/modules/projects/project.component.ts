fdb705a44c7203df6cda3f297681ba0a134598c1
Barbara <barbara.reliefapps@gmail.com>
2019-02-15 10:19:38 +0700

    fix project component

--- src/app/modules/projects/project.component.ts
+++ src/app/modules/projects/project.component.ts
@@ -15,297 +15,297 @@ import { ExportInterface } from '../../model/export.interface';
 import { ModalAddComponent } from '../../components/modals/modal-add/modal-add.component';
 import { AsyncacheService } from 'src/app/core/storage/asyncache.service';
 import { delay, finalize } from 'rxjs/operators';
-import { ImportedDataService} from '../../core/utils/imported-data.service';
+import { ImportedDataService } from '../../core/utils/imported-data.service';
 import { throwError, Subscriber, Subscription } from 'rxjs';
 import { CarouselComponent } from 'src/app/components/carousel/carousel.component';
 import { SlideSelectorService } from 'src/app/core/utils/slide-selector.service';
 
 @Component({
-    selector: 'app-project',
-    templateUrl: './project.component.html',
-    styleUrls: ['./project.component.scss']
+	selector: 'app-project',
+	templateUrl: './project.component.html',
+	styleUrls: ['./project.component.scss']
 })
 export class ProjectComponent implements OnInit {
-    @ViewChild(CarouselComponent) carousel: CarouselComponent;
+	@ViewChild(CarouselComponent) carousel: CarouselComponent;
 
-    public nameComponent = 'projects';
-    public distribution = GlobalText.TEXTS;
-    public language = GlobalText.language;
-    loadingExport = false;
+	public nameComponent = 'projects';
+	public distribution = GlobalText.TEXTS;
+	public language = GlobalText.language;
+	loadingExport = false;
 
-    projects: Project[];
-    distributionData: MatTableDataSource<any>;
-    distributionClass = DistributionData;
-    projectClass = Project;
+	projects: Project[];
+	distributionData: MatTableDataSource<any>;
+	distributionClass = DistributionData;
+	projectClass = Project;
 
-    // loading
-    loadingDistributions = true;
-    loadingProjects = true;
-    noNetworkData = false;
+	// loading
+	loadingDistributions = true;
+	loadingProjects = true;
+	noNetworkData = false;
 
-    selectedTitle = '';
-    selectedProject = null;
-    selectedProjectId = null;
-    isBoxClicked = false;
-    extensionType: string;
-    hasRights: boolean = false;
-    hasRightsEdit: boolean = false;
+	selectedTitle = '';
+	selectedProject = null;
+	selectedProjectId = null;
+	isBoxClicked = false;
+	extensionType: string;
+	hasRights: boolean = false;
+	hasRightsEdit: boolean = false;
 
-    public maxHeight = GlobalText.maxHeight;
-    public maxWidthMobile = GlobalText.maxWidthMobile;
-    public maxWidthFirstRow = GlobalText.maxWidthFirstRow;
-    public maxWidthSecondRow = GlobalText.maxWidthSecondRow;
-    public maxWidth = GlobalText.maxWidth;
-    public heightScreen;
-    public widthScreen;
-    public slides: Object[] = [];
-    private slideSubscriber: Subscription;
+	public maxHeight = GlobalText.maxHeight;
+	public maxWidthMobile = GlobalText.maxWidthMobile;
+	public maxWidthFirstRow = GlobalText.maxWidthFirstRow;
+	public maxWidthSecondRow = GlobalText.maxWidthSecondRow;
+	public maxWidth = GlobalText.maxWidth;
+	public heightScreen;
+	public widthScreen;
+	public slides: Object[] = [];
+	private slideSubscriber: Subscription;
 
-    constructor(
-        public projectService: ProjectService,
-        public distributionService: DistributionService,
-        public mapperService: Mapper,
-        private router: Router,
-        private _cacheService: AsyncacheService,
-        public snackBar: MatSnackBar,
-        public dialog: MatDialog,
-        public importedDataService: ImportedDataService,
-        private slideSelectorService: SlideSelectorService,
-    ) { }
+	constructor(
+		public projectService: ProjectService,
+		public distributionService: DistributionService,
+		public mapperService: Mapper,
+		private router: Router,
+		private _cacheService: AsyncacheService,
+		public snackBar: MatSnackBar,
+		public dialog: MatDialog,
+		public importedDataService: ImportedDataService,
+		private slideSelectorService: SlideSelectorService,
+	) { }
 
-    ngOnInit() {
-        this.slideSubscriber = this.slideSelectorService.selectedSlide.subscribe((slide: any) => {
-            if (slide && slide.project) {
-                this.selectTitle(slide.project.name, slide.project);
-            }
-        });
+	ngOnInit() {
+		this.slideSubscriber = this.slideSelectorService.selectedSlide.subscribe((slide: any) => {
+			if (slide && slide.project) {
+				this.selectTitle(slide.project.name, slide.project);
+			}
+		});
 
-        if (this.importedDataService.emittedProject) {
-            this.selectedProjectId = parseInt(this.importedDataService.project)
-            this.getProjects();
-        }
-        else {
-            this.getProjects();
-        }
-        this.checkSize();
-        this.checkPermission();
-        this.extensionType = 'xls';
-    }
+		if (this.importedDataService.emittedProject) {
+			this.selectedProjectId = parseInt(this.importedDataService.project)
+			this.getProjects();
+		}
+		else {
+			this.getProjects();
+		}
+		this.checkSize();
+		this.checkPermission();
+		this.extensionType = 'xls';
+	}
 
-    ngOnDestroy(): void {
-        this.slideSubscriber.unsubscribe();
-    }
+	ngOnDestroy(): void {
+		this.slideSubscriber.unsubscribe();
+	}
 
-    @HostListener('window:resize', ['$event'])
-    onResize(event) {
-        this.checkSize();
-    }
+	@HostListener('window:resize', ['$event'])
+	onResize(event) {
+		this.checkSize();
+	}
 
-    checkSize(): void {
-        this.heightScreen = window.innerHeight;
-        this.widthScreen = window.innerWidth;
-    }
+	checkSize(): void {
+		this.heightScreen = window.innerHeight;
+		this.widthScreen = window.innerWidth;
+	}
 
     /**
      * check if the langage has changed
      */
-    ngDoCheck() {
-        if (this.distribution !== GlobalText.TEXTS) {
-            this.distribution = GlobalText.TEXTS;
-            this.nameComponent = GlobalText.TEXTS.distributions;
-        }
+	ngDoCheck() {
+		if (this.distribution !== GlobalText.TEXTS) {
+			this.distribution = GlobalText.TEXTS;
+			this.nameComponent = GlobalText.TEXTS.distributions;
+		}
 
-        if (this.language !== GlobalText.language) {
-            this.language = GlobalText.language;
-        }
-    }
+		if (this.language !== GlobalText.language) {
+			this.language = GlobalText.language;
+		}
+	}
 
 
-    public selectSlide(slide: any) {
-        this.selectTitle(slide.ref);
-    }
+	public selectSlide(slide: any) {
+		this.selectTitle(slide.ref);
+	}
 
     /**
      * update current project and its distributions when a other project box is clicked
      * @param title
      * @param project
      */
-    selectTitle(title, project?): void {
+	selectTitle(title, project?): void {
 
-        this.isBoxClicked = true;
-        this.selectedTitle = title;
-        this.selectedProject = project;
-        this.loadingDistributions = true;
-        this.getDistributionsByProject(project.id);
-    }
+		this.isBoxClicked = true;
+		this.selectedTitle = title;
+		this.selectedProject = project;
+		this.loadingDistributions = true;
+		this.getDistributionsByProject(project.id);
+	}
 
-    setType(choice: string) {
-        this.extensionType = choice;
-    }
+	setType(choice: string) {
+		this.extensionType = choice;
+	}
 
-    // TO DO : get from cache
+	// TO DO : get from cache
     /**
      * get all projects
      */
-    getProjects(): void {
-        this.projectService.get().pipe(
-            finalize(
-                () => {
-                    this.loadingProjects = false;
-                },
-            )
-        ).subscribe(
-            response => {
-                if (response && response.length > 0) {
-                    this.projects = this.projectClass.formatArray(response).reverse();
-                    this.generateProjectsSlide();
-
-                    if (this.carousel) {
-                        this.carousel.update();
-                    }
-                    if (this.selectedProjectId) {
-                        this.autoProjectSelect(this.selectedProjectId);
-                    } else {
-                        this.selectTitle(this.projects[0].name, this.projects[0]);
-                    }
-            }
-        );
-    }
+	getProjects(): void {
+		this.projectService.get().pipe(
+			finalize(
+				() => {
+					this.loadingProjects = false;
+				},
+			)
+		).subscribe(
+			response => {
+				if (response && response.length > 0) {
+					this.projects = this.projectClass.formatArray(response).reverse();
+					this.generateProjectsSlide();
+					if (this.carousel) {
+						this.carousel.update();
+					}
+					if (this.selectedProjectId) {
+						this.autoProjectSelect(this.selectedProjectId);
+					} else {
+						this.selectTitle(this.projects[0].name, this.projects[0]);
+					}
+				}
+			}
+		);
+	}
 
     /**
      * get all distributions of a project
      * @param projectId
      */
-    getDistributionsByProject(projectId: number): void {
-        this.distributionService.
-            getByProject(projectId).pipe(
-                finalize(
-                    () => {
-                        this.loadingDistributions = false;
-                    },
-                )
-            ).subscribe(
-                response => {
-                    if (response || response === []) {
-                        this.noNetworkData = false;
-                        const distribution = DistributionData.formatArray(response);
-                        this.loadingDistributions = false;
+	getDistributionsByProject(projectId: number): void {
+		this.distributionService.
+			getByProject(projectId).pipe(
+				finalize(
+					() => {
+						this.loadingDistributions = false;
+					},
+				)
+			).subscribe(
+				response => {
+					if (response || response === []) {
+						this.noNetworkData = false;
+						const distribution = DistributionData.formatArray(response);
+						this.loadingDistributions = false;
 
-                        this.distributionData = new MatTableDataSource(distribution);
-                    } else {
-                        this.distributionData = null;
-                        this.loadingDistributions = false;
-                        this.noNetworkData = true;
-                    }
-                }
-            )
-        // .catch(
-        //     error => {
-        //         this.distributionData = null;
-        //         this.noNetworkData = true;
-        //     }
-        // )
-    }
+						this.distributionData = new MatTableDataSource(distribution);
+					} else {
+						this.distributionData = null;
+						this.loadingDistributions = false;
+						this.noNetworkData = true;
+					}
+				}
+			)
+		// .catch(
+		//     error => {
+		//         this.distributionData = null;
+		//         this.noNetworkData = true;
+		//     }
+		// )
+	}
 
-    addDistribution() {
-        this.router.navigate(['projects/add-distribution'], { queryParams: { project: this.selectedProject.id } });
-    }
+	addDistribution() {
+		this.router.navigate(['projects/add-distribution'], { queryParams: { project: this.selectedProject.id } });
+	}
 
     /**
      * Export distribution data
      */
-    export() {
-        this.loadingExport = true;
-        this.distributionService.export('project', this.extensionType, this.selectedProject.id).then(
-            () => { this.loadingExport = false }
-        ).catch(
-            () => { this.loadingExport = false }
-        )
-    }
+	export() {
+		this.loadingExport = true;
+		this.distributionService.export('project', this.extensionType, this.selectedProject.id).then(
+			() => { this.loadingExport = false }
+		).catch(
+			() => { this.loadingExport = false }
+		)
+	}
 
-    openNewProjectDialog() {
-        const dialogRef = this.dialog.open(
-            ModalAddComponent, {
-                data: {
-                    data: [],
-                    entity: Project,
-                    service: this.projectService,
-                    mapper: this.mapperService
-                }
-            }
-        );
-        const create = dialogRef.componentInstance.onCreate.subscribe(
-            (data) => {
-                let exists: boolean = false;
-                if (this.projects) {
-                  this.projects.forEach(element => {
-                    if (element.name.toLowerCase() == data.name.toLowerCase()) {
-                      this.snackBar.open(this.distribution.settings_project_exists, '', { duration: 5000, horizontalPosition: 'right' });
-                      exists = true;
-                      return;
-                    }
-                  });
-                }
+	openNewProjectDialog() {
+		const dialogRef = this.dialog.open(
+			ModalAddComponent, {
+				data: {
+					data: [],
+					entity: Project,
+					service: this.projectService,
+					mapper: this.mapperService
+				}
+			}
+		);
+		const create = dialogRef.componentInstance.onCreate.subscribe(
+			(data) => {
+				let exists: boolean = false;
+				if (this.projects) {
+					this.projects.forEach(element => {
+						if (element.name.toLowerCase() == data.name.toLowerCase()) {
+							this.snackBar.open(this.distribution.settings_project_exists, '', { duration: 5000, horizontalPosition: 'right' });
+							exists = true;
+							return;
+						}
+					});
+				}
 
-                if (exists == false)
-                    this.createElement(data);
-            }
-        );
-    }
+				if (exists == false)
+					this.createElement(data);
+			}
+		);
+	}
 
-    createElement(createElement: Object) {
-        createElement = Project.formatForApi(createElement);
-        this.projectService.create(createElement['id'], createElement).subscribe(response => {
-            this.snackBar.open("Project " + this.distribution.settings_created, '', { duration: 5000, horizontalPosition: 'right' });
-            this.getProjects();
-        });
-        
-    }
+	createElement(createElement: Object) {
+		createElement = Project.formatForApi(createElement);
+		this.projectService.create(createElement['id'], createElement).subscribe(response => {
+			this.snackBar.open("Project " + this.distribution.settings_created, '', { duration: 5000, horizontalPosition: 'right' });
+			this.getProjects();
+		});
 
-    checkPermission() {
-        this._cacheService.getUser().subscribe(
-            result => {
-                const rights = result.rights;
-                if (rights == "ROLE_ADMIN" || rights == 'ROLE_PROJECT_MANAGER')
-                    this.hasRights = true;
+	}
 
-                if (rights == "ROLE_ADMIN" || rights == 'ROLE_PROJECT_MANAGER' || rights == "ROLE_PROJECT_OFFICER")
-                    this.hasRightsEdit = true;
-            }
-        )
-    }
+	checkPermission() {
+		this._cacheService.getUser().subscribe(
+			result => {
+				const rights = result.rights;
+				if (rights == "ROLE_ADMIN" || rights == 'ROLE_PROJECT_MANAGER')
+					this.hasRights = true;
 
-    autoProjectSelect(input: string) {
-        let selector = parseInt(input)
-        this.slides.forEach((slide: any) => {
-            if (slide.project.id === selector) {
-                this.slideSelectorService.selectSlide(slide);
-                return;
-            }
-        })
-    }
+				if (rights == "ROLE_ADMIN" || rights == 'ROLE_PROJECT_MANAGER' || rights == "ROLE_PROJECT_OFFICER")
+					this.hasRightsEdit = true;
+			}
+		)
+	}
 
-    private generateProjectsSlide(): void {
-        this.resetSlides();
-        if (this.projects) {
-            this.projects.forEach(project => {
-                this.slides.push(
-                    {
-                        slideInfo: {
-                            icon: "settings/projects",
-                            color: "green",
-                            title: project.name,
-                            ref: project.name,
-                        },
-                        project: project,
-                    }
-                );
-            });
-            this.slideSelectorService.setSlides(this.slides);
-        }
-    }
+	autoProjectSelect(input: string) {
+		let selector = parseInt(input)
+		this.slides.forEach((slide: any) => {
+			if (slide.project.id === selector) {
+				this.slideSelectorService.selectSlide(slide);
+				return;
+			}
+		})
+	}
 
-    private resetSlides(): void {
-        this.slides = [];
-    }
+	private generateProjectsSlide(): void {
+		this.resetSlides();
+		if (this.projects) {
+			this.projects.forEach(project => {
+				this.slides.push(
+					{
+						slideInfo: {
+							icon: "settings/projects",
+							color: "green",
+							title: project.name,
+							ref: project.name,
+						},
+						project: project,
+					}
+				);
+			});
+			this.slideSelectorService.setSlides(this.slides);
+		}
+	}
+
+	private resetSlides(): void {
+		this.slides = [];
+	}
 }

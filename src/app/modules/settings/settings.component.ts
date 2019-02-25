5bb0a24c8804eec0ec9f0e28d86f416b579060bc
Grégoire Rabasse <gregoire.reliefapps@gmail.com>
2019-02-25 10:45:54 +0100

    really fixed merge conflict

--- src/app/modules/settings/settings.component.ts
+++ src/app/modules/settings/settings.component.ts
@@ -215,7 +215,9 @@ export class SettingsComponent implements OnInit, DoCheck {
               element.country = '';
 
               for (let i = 0; i < element.user_projects.length; i++) {
-                element.projects[i] = element.user_projects[i].project.name;
+                if (element.user_projects[i].project) {
+                  element.projects[i] = element.user_projects[i].project.name;
+                }
               }
               for (let i = 0; i < element.countries.length; i++) {
                 element.country = element.countries[i].iso3;

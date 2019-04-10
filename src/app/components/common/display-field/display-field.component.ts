80b5f46cdd6fab197a3c4aee667a1b22d685a841
developer3 <coline.therial@orange.fr>
2019-04-10 18:39:19 +0200

    Refacto box properties, table, and edit not validated distribution

---
+++ src/app/components/common/display-field/display-field.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-display-field',
+  templateUrl: './display-field.component.html',
+  styleUrls: ['display-field.component.scss']
+})
+export class DisplayFieldComponent implements OnInit {
+
+  @Input() field;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

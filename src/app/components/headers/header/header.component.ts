6911e8c6c5f75a41b6835f072169466097650309
Stanislas Bruhiere <stanbill91@gmail.com>
2019-04-30 14:58:47 +0200

    Start implementing breadcrumbs logic

--- src/app/components/headers/header/header.component.ts
+++ src/app/components/headers/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material';
+import { Event, NavigationStart, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { CountriesService } from 'src/app/core/countries/countries.service';
 import { AsyncacheService } from 'src/app/core/storage/asyncache.service';
@@ -25,15 +26,25 @@ export class HeaderComponent implements OnInit, OnDestroy {
     public countries: Array<Country>;
     private subscriptions: Array<Subscription>;
 
+    // Breadcrumbs
+    public breadcrumbs: Array<string>;
+
     constructor(
         private dialog: MatDialog,
         private languageService: LanguageService,
         private asynCacheService: AsyncacheService,
         private countriesService: CountriesService,
+        private router: Router,
     ) {
 
     }
     ngOnInit(): void {
+        this.router.events.subscribe((event: Event) => {
+            console.log(event);
+            if (event instanceof NavigationStart) {
+                this.updateBreadCrumbs(event.url);
+            }
+        });
         this.subscriptions = [
                 this.countriesService.selectedCountry.subscribe((country: Country) => {
                     this.selectedCountry = country;
@@ -42,6 +53,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
                     this.countries = countries;
                 }),
         ];
+        this.updateBreadCrumbs(this.router.url);
     }
 
     ngOnDestroy(): void {
@@ -68,4 +80,15 @@ export class HeaderComponent implements OnInit, OnDestroy {
     openLanguageDialog() {
         this.dialog.open(ModalLanguageComponent, {});
     }
+
+//
+// ─── BREADCRUMBS ────────────────────────────────────────────────────────────────
+//
+    updateBreadCrumbs(url: string) {
+        this.breadcrumbs = url.split('/');
+        console.log(this.breadcrumbs);
+    }
+
+
+
 }

7ba0bf6db7a138c913101daee2365bcff88e2909
Stanislas Bruhiere <stanbill91@gmail.com>
2019-05-07 10:58:13 +0200

    Fix box settings components overrun.
    - Implement screen size service in box settings.
    - Reduce max number of characters in mobile mode.
    - Improve readability.
    
    This commit closes #601.

--- src/app/components/box/box-setting/box-setting.component.ts
+++ src/app/components/box/box-setting/box-setting.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ScreenSizeService } from 'src/app/core/screen-size/screen-size.service';
+import { DisplayType } from 'src/constants/screen-sizes';
 
 @Component({
     selector: 'app-box-setting',
     templateUrl: './box-setting.component.html',
     styleUrls: ['./box-setting.component.scss']
 })
-export class BoxSettingComponent {
+export class BoxSettingComponent implements OnInit, OnDestroy {
     @Input() isSelected: boolean;
     // Todo: Remove this
     @Input() selectedTitle: string;
@@ -14,4 +17,30 @@ export class BoxSettingComponent {
     @Output() emitClickedEntity = new EventEmitter<any>();
 
     readonly MAX_PROP_LENGTH = 35;
+    readonly MAX_PROP_LENGTH_MOBILE = 25;
+
+    public maxLength = this.MAX_PROP_LENGTH;
+
+    // Screen size
+    public currentDisplayType: DisplayType;
+    private screenSizeSubscription: Subscription;
+
+    constructor(
+        private screenSizeService: ScreenSizeService,
+    ) { }
+
+    ngOnInit() {
+        this.screenSizeSubscription = this.screenSizeService.displayTypeSource.subscribe((displayType: DisplayType) => {
+            this.currentDisplayType = displayType;
+            if (this.currentDisplayType.type === 'mobile') {
+                this.maxLength = this.MAX_PROP_LENGTH_MOBILE;
+            } else {
+                this.maxLength = this.MAX_PROP_LENGTH;
+            }
+        });
+    }
+
+    ngOnDestroy() {
+        this.screenSizeSubscription.unsubscribe();
+    }
 }

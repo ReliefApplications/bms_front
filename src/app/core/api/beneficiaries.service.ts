3738189ecae9d00655fc02b3485a9e489a616eaf
developer3 <coline.therial@orange.fr>
2019-05-31 09:48:38 +0200

    PR comment

--- src/app/core/api/beneficiaries.service.ts
+++ src/app/core/api/beneficiaries.service.ts
@@ -27,7 +27,7 @@ export class BeneficiariesService extends CustomModelService {
     }
 
     public delete(beneficiaryId: number, distributionId: any, justification: string) {
-        const url = this.api + '/delete-beneficiaries/' + beneficiaryId + '?distribution=' + distributionId;
+        const url = this.api + '/distribution/beneficiaries/' + beneficiaryId + '/delete?distribution=' + distributionId;
         const body = {
             justification: justification
         };

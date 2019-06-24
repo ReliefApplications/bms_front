c9b2196e078693032355662d5441871e91bd21c5
Stanislas Bruhiere <stanbill91@gmail.com>
2019-06-24 09:21:09 +0200

    Edit markers and popups

---
+++ src/app/core/external/distribution-marker.service.ts
@@ -0,0 +1,51 @@
+import { DatePipe } from '@angular/common';
+import { Injectable } from '@angular/core';
+import * as Leaflet from 'leaflet';
+import { Commodity } from 'src/app/models/commodity';
+import { Distribution } from 'src/app/models/distribution';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DistributionMarkerService {
+    datePipe = new DatePipe('en-US');
+    public getClassesNames(distribution: Distribution) {
+        return this.getDistributionStatus(distribution);
+    }
+
+    private getDistributionStatus(distribution: Distribution) {
+        if (!distribution.get<boolean>('validated')) {
+            return 'not-validated';
+        }
+        if (distribution.get<boolean>('completed')) {
+            return 'completed';
+        }
+        return 'validated';
+    }
+
+    public isToday(distribution: Distribution) {
+        const today = new Date();
+        const distributionDate = distribution.get<Date>('date');
+        today.setHours(0, 0, 0, 0);
+        distributionDate.setHours(0, 0, 0, 0);
+        return (distributionDate.getTime() === today.getTime());
+    }
+
+    public getImage(distribution: Distribution) {
+        return distribution.get<Array<Commodity>>('commodities')[0].getImage();
+    }
+
+    public getPopup(distribution: Distribution) {
+        const popup = Leaflet.DomUtil.create('div', 'infoWindow');
+
+        popup.innerHTML = `
+            <div id="bms-popup">
+                <p>Name: ${distribution.get<string>('name')}</p>
+                <p>Status: ${this.getDistributionStatus(distribution)}</p>
+                <p>Date: ${ this.datePipe.transform(distribution.get<Date>('date'), 'dd-MM-yyyy')}</p>
+            </div>
+        `;
+        return popup;
+    }
+}
--- src/app/core/external/distribution-marker.ts
+++ src/app/core/external/distribution-marker.ts
@@ -1,6 +1,8 @@
 import * as Leaflet from 'leaflet';
 import 'leaflet.markercluster';
+import { AppInjector } from 'src/app/app-injector';
 import { Distribution } from 'src/app/models/distribution';
+import { DistributionMarkerService } from './distribution-marker.service';
 
 export class DistributionMarker {
 
@@ -9,13 +11,23 @@ export class DistributionMarker {
     marker: Leaflet.Layer;
     popup: HTMLElement;
 
-    popupIsOpen = false;
+    private readonly iconSize = {
+        width: 40,
+        height: 56,
+    };
 
-    constructor (area: Leaflet.FeatureGroup, map: Leaflet.Map, distribution: Distribution) {
+    popupIsOpen = false;
+    private distributionMarkerService: DistributionMarkerService;
+
+    constructor (area: Leaflet.FeatureGroup, map: Leaflet.Map, distribution: Distribution,
+        ) {
+
+        this.distributionMarkerService = AppInjector.get(DistributionMarkerService);
+
         this.area = area;
         this.map = map;
          // Calculate their center
-         this.marker = Leaflet.marker(this.area.getBounds().getCenter(), {icon: this.generateIcon() });
+         this.marker = Leaflet.marker(this.area.getBounds().getCenter(), {icon: this.generateIcon(distribution) });
 
         this.area.setStyle(this.generateAreaStyle());
 
@@ -110,8 +122,7 @@ export class DistributionMarker {
     }
 
     private addPopup(distribution: Distribution) {
-        this.popup = Leaflet.DomUtil.create('div', 'infoWindow');
-        this.popup.innerHTML = `<p id="bms-popup">${distribution.get<string>('name')}</p>`;
+        this.popup = this.distributionMarkerService.getPopup(distribution);
         this.marker.bindPopup(this.popup);
     }
 
@@ -140,12 +151,28 @@ export class DistributionMarker {
         };
     }
 
-    private generateIcon() {
-        return Leaflet.icon({
-            iconUrl: 'assets/maps/marker.png',
-            iconSize:       [74, 74], // size of the icon
-            iconAnchor:     [37, 74], // position of the icon
-            popupAnchor:    [0 , -30],
+    private generateIcon(distribution: Distribution) {
+        const {width, height} = this.iconSize;
+        const status = this.distributionMarkerService.getClassesNames(distribution);
+        return Leaflet.divIcon({
+            className: `marker-container ${status}`,
+            html: `
+                <object class="marker-icon" width="20" height="20" fill="white"
+                    data="${this.distributionMarkerService.getImage(distribution)}">
+
+                </object>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 280" >
+                    <circle class="marker-pointer" cx="100" cy="100" r="85" stroke="none"/>
+                    <path class="marker-pointer" d="M 100 280 L 13 150 Q 100 249 186 150 Z" stroke="none"/>
+                    ${this.distributionMarkerService.isToday(distribution) ? '<animate attributeType="CSS" attributeName="opacity"\
+                    values="1;0.5;1" dur="1s" repeatCount="indefinite" />' : ''}
+                </svg>
+            `,
+            // iconUrl: 'assets/maps/marker.svg',
+            // // 0,0 is the top left corner of the icon
+            iconSize:       [width, height], // size of the icon
+            iconAnchor:     [width / 2, height], // Position of the location relative to the icon
+            popupAnchor:    [0, - height + 5], // Position of the popup relative to the iconAnchor
         });
     }
 }

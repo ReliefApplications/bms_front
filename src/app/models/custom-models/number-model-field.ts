6fb5c7c1233cdbd4ce2233820f4442694f8d82be
developer3 <coline.therial@orange.fr>
2019-07-05 10:41:25 +0200

    Add currency to value column in qr voucher distribution

--- src/app/models/custom-models/number-model-field.ts
+++ src/app/models/custom-models/number-model-field.ts
@@ -2,4 +2,13 @@ import { CustomModelField } from './custom-model-field';
 
 export class NumberModelField extends CustomModelField<number> {
     kindOfField = 'Number';
+      /* The value to display (WARNING: can be set only from the api, not a modifiable field)
+     * @type {string}
+     */
+    displayValue: string;
+
+    constructor(properties: any) {
+        super(properties);
+        this.displayValue             = properties['displayValue'];
+    }
 }

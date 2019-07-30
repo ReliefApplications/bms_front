1051cab864b0ea49fa4bf773a5386d5a35482440
Jaime Rodriguez <jaime.reliefapps@gmail.com>
2019-07-30 16:57:48 +0200

    Code refractored

--- src/app/modules/logs/logs.component.ts
+++ src/app/modules/logs/logs.component.ts
@@ -95,109 +95,106 @@ selectTab(tab: string): void {
 createGraph() {
   const sortedLogs = this.logs
     .sort((log1: Log, log2: Log) => log1.get<Date>('date').getTime() - log2.get<Date>('date').getTime());
-    const oldestDate = sortedLogs[0].get<Date>('date').getFullYear();
-    const latestDate = sortedLogs[sortedLogs.length - 1].get<Date>('date').getFullYear();
+  const oldestDate = sortedLogs[0].get<Date>('date').getFullYear();
+  const latestDate = sortedLogs[sortedLogs.length - 1].get<Date>('date').getFullYear();
+  const oldestDay = new Date();
+    oldestDay.setDate(oldestDay.getDate() - 90);
+  const latestDay = new Date();
+    latestDay.setDate(latestDay.getDate() + 1);
+  const datePipe = new DatePipe('en-US');
 
-    const statusGraph = {
+  const statusGraph = {
       type: 'pie',
       name: 'Success/Error Rate',  // this.language.something
       values: {}
-    };
+  };
+
+  const dayRequestGraph = {
+    type: 'line',
+    name: 'Requests per day',
+    yLabel: 'Requests',
+    values: {}
+  };
+
+  const activeUsersGraph = {
+    type: 'bar',
+    name: 'Most active users',
+    xLabel: 'Users',
+    yLabel: 'Requests',
+    values: {}
+  };
+
+  this.requestsKHM = {
+    type: 'line',
+    name: 'Requests KHM',
+    xLabel: 'Time',
+    yLabel: 'Requests',
+    values: {},
+  };
+
+  this.requestsSYR = {
+    type: 'line',
+    name: 'Requests SYR',
+    xLabel: 'Time',
+    yLabel: 'Requests',
+    values: {},
+  };
 
     for (let date = oldestDate; date <= latestDate; date++) {
       statusGraph.values[date] = [{date: date, name: 'success', unit: 'success', value: 0},
         {date: date, name: 'errors', unit: 'errors', value: 0}];
     }
+
+    for (const date = new Date(oldestDay.getTime()); date <= latestDay; date.setDate(date.getDate() + 1)) {
+      const formatDatePipe = datePipe.transform(date, 'dd-MM-yyyy');
+      dayRequestGraph.values[formatDatePipe] =
+        [{date: formatDatePipe, name: 'requests', unit: 'requests', value: 0}];
+      this.requestsKHM.values[formatDatePipe] =
+        [{date: formatDatePipe, name: 'requests', unit: 'requests', value: 0}];
+      this.requestsSYR.values[formatDatePipe] =
+        [{date: formatDatePipe, name: 'requests', unit: 'requests', value: 0}];
+    }
+
     this.logs.forEach((log: Log) => {
+      const formatDatePipe = datePipe.transform(log.get<Date>('date'), 'dd-MM-yyyy');
+      // Status (success/errors) pie chart
       if (log.get<string>('status') === 'success') {
         statusGraph.values[log.get<Date>('date').getFullYear()][0].value++;
       } else {
         statusGraph.values[log.get<Date>('date').getFullYear()][1].value++;
       }
-    });
-    this.graphs.push(statusGraph);
-
-    const dayRequestGraph = {
-      type: 'line',
-      name: 'Requests per day',
-      yLabel: 'Requests',
-      values: {}
-    };
-
-    const oldestDay = new Date();
-    oldestDay.setDate(oldestDay.getDate() - 90);
-    const latestDay = new Date();
-    latestDay.setDate(latestDay.getDate() + 1);
-    const datePipe = new DatePipe('en-US');
-
-    for (const date = new Date(oldestDay.getTime()); date <= latestDay; date.setDate(date.getDate() + 1)) {
-      dayRequestGraph.values[datePipe.transform(date, 'dd-MM-yyyy')] =
-        [{date: datePipe.transform(date, 'dd-MM-yyyy'), name: 'requests', unit: 'requests', value: 0}];
-    }
-
-    this.logs.forEach((log: Log) => {
-      if (dayRequestGraph.values[datePipe.transform(log.get<Date>('date'), 'dd-MM-yyyy')]) {
-      dayRequestGraph.values[datePipe.transform(log.get<Date>('date'), 'dd-MM-yyyy')][0].value++;
+      // Request per day line chart
+      if (dayRequestGraph.values[formatDatePipe]) {
+        dayRequestGraph.values[formatDatePipe][0].value++;
       }
-    });
-    this.graphs.push(dayRequestGraph);
-
-    const activeUsersGraph = {
-      type: 'bar',
-      name: 'Most active users',
-      xLabel: 'Users',
-      yLabel: 'Requests',
-      values: {},
-    };
-
-    this.logs.forEach((log: Log) => {
+      // Most active users bar chart
       if (activeUsersGraph.values[log.get<string>('user')]) {
         activeUsersGraph.values[log.get<string>('user')][0].value++;
       } else {
         activeUsersGraph.values[log.get<string>('user')] = [{name: log.get<string>('user'), value: 1, unit: 'requests'}];
       }
+      // Requests per country line chart
+      if (this.requestsKHM.values[formatDatePipe]) {
+        if (log.get<string>('country') === 'KHM') {
+          this.requestsKHM.values[formatDatePipe][0].value++;
+        }
+      }
+      if (this.requestsSYR.values[formatDatePipe]) {
+        if (log.get<string>('country') === 'SYR') {
+          this.requestsSYR.values[formatDatePipe][0].value++;
+        }
+      }
     });
+
+    // Sort most active users by their amount of requests
     activeUsersGraph.values = Object.keys(activeUsersGraph.values)
       .sort((user1, user2) => activeUsersGraph.values[user2][0].value - activeUsersGraph.values[user1][0].value)
         .reduce((_sortedObj, key) => ({..._sortedObj, [key]: activeUsersGraph.values[key]}), {});
 
+    // Push the graphs into the list of graphs
+    this.graphs.push(statusGraph);
+    this.graphs.push(dayRequestGraph);
     this.graphs.push(activeUsersGraph);
-
-    this.requestsKHM = {
-      type: 'line',
-      name: 'Requests KHM',
-      xLabel: 'Time',
-      yLabel: 'Requests',
-      values: {},
-    };
-
-    this.requestsSYR = {
-      type: 'line',
-      name: 'Requests SYR',
-      xLabel: 'Time',
-      yLabel: 'Requests',
-      values: {},
-    };
-
-    for (const date = new Date(oldestDay.getTime()); date <= latestDay; date.setDate(date.getDate() + 1)) {
-      this.requestsKHM.values[datePipe.transform(date, 'dd-MM-yyyy')] =
-        [{date: datePipe.transform(date, 'dd-MM-yyyy'), name: 'requests', unit: 'requests', value: 0}];
-      this.requestsSYR.values[datePipe.transform(date, 'dd-MM-yyyy')] =
-        [{date: datePipe.transform(date, 'dd-MM-yyyy'), name: 'requests', unit: 'requests', value: 0}];
-    }
-
-    this.logs.forEach((log: Log) => {
-      if (this.requestsKHM.values[datePipe.transform(log.get<Date>('date'), 'dd-MM-yyyy')]) {
-        if (log.get<string>('country') === 'KHM') {
-          this.requestsKHM.values[datePipe.transform(log.get<Date>('date'), 'dd-MM-yyyy')][0].value++;
-        }
-      }
-      if (this.requestsSYR.values[datePipe.transform(log.get<Date>('date'), 'dd-MM-yyyy')]) {
-        if (log.get<string>('country') === 'SYR') {
-          this.requestsSYR.values[datePipe.transform(log.get<Date>('date'), 'dd-MM-yyyy')][0].value++;
-        }
-      }
-    });
 }
 
 

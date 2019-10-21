7e81b2d7ad3b7297da06571200052be2d85cea6d
Jaime Rodriguez <jaime.reliefapps@gmail.com>
2019-10-21 09:46:49 +0200

    Fixed #2

--- src/app/modules/beneficiaries/update-beneficiary/update-beneficiary.component.ts
+++ src/app/modules/beneficiaries/update-beneficiary/update-beneficiary.component.ts
@@ -667,19 +667,6 @@ export class UpdateBeneficiaryComponent implements OnInit, DesactivationGuarded,
                     locationMessage = this.language.beneficiary_error_location;
                 } else if (!controls[locationGroup + 'Type'].value) {
                     locationMessage = this.language.beneficiairy_error_location_type;
-                } else if (controls[locationGroup + 'Type'].value !== 'camp' && !controls[locationGroup + 'AddressNumber'].value) {
-                    locationMessage = this.language.beneficiairy_error_address_number;
-                } else if (controls[locationGroup + 'Type'].value !== 'camp' && !controls[locationGroup + 'AddressStreet'].value) {
-                    locationMessage = this.language.beneficiary_error_address_street;
-                } else if (controls[locationGroup + 'Type'].value !== 'camp' && !controls[locationGroup + 'AddressPostcode'].value) {
-                    locationMessage = this.language.beneficiary_error_address_postcode;
-                } else if (controls[locationGroup + 'Type'].value === 'camp' &&
-                    ((!controls[locationGroup + 'CreateCamp'].value && !controls[locationGroup + 'Camp'].value) ||
-                        (controls[locationGroup + 'CreateCamp'].value && !controls[locationGroup + 'NewCamp'].value))
-                ) {
-                    locationMessage = this.language.beneficiary_error_camp;
-                } else if (controls[locationGroup + 'Type'].value === 'camp' && !controls.currentTentNumber.value) {
-                    locationMessage = this.language.beneficiary_error_tent;
                 }
             }
         });
